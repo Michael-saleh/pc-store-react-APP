54f7fbd987ce1a27be410147a032e78cd8e7a91e
michael-saleh <michael-saleh@live.com>
2025-06-17 00:40:06 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './App/store';
+import { store } from './App/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
